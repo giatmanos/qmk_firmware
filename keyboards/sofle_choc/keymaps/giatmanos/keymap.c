47561b4f14080c75ed9361e0eb23481ce734c825
Manos Giatrakis <manos.giatrakis.dev@pm.m>
2024-11-01 22:27:59 +0000

    Configured ModTap for the home row

--- keyboards/sofle_choc/keymaps/giatmanos/keymap.c
+++ keyboards/sofle_choc/keymaps/giatmanos/keymap.c
@@ -50,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_DEL,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+    KC_TAB,ALT_T(KC_A),GUI_T(KC_S),SFT_T(KC_D),CTL_T(KC_F),KC_G,                    KC_H,CTL_T(KC_J),SFT_T(KC_K),GUI_T(KC_L),ALT_T(KC_SCLN), KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_ESC,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    KC_MPLY,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  XXXXXXX,
+      KC_ESC,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MPLY,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-               KC_LGUI, KC_LALT,MO(_NUMS),TD(TD_SFT_CTRL),   KC_ENT,   KC_SPC, TD(TD_CTRL_SFT),  MO(_SYMB), KC_LALT, TG(_NPAD)
+               KC_LGUI, KC_LALT, KC_LBRC,      MO(_NUMS),   KC_ENT,    KC_SPC, MO(_SYMB), KC_RBRC, KC_LALT, TG(_NPAD)
 ),
 [_NUMS] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
