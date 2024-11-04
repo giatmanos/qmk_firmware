bd0a6e149e26d724dafd6f85e8375c1d9d4e3a0e
Manos Giatrakis <manos.giatrakis.dev@pm.m>
2024-11-04 12:35:51 +0000

    Revert "Configured ModTap for the home row"
    This reverts commit 47561b4f14080c75ed9361e0eb23481ce734c825.

--- keyboards/crkbd/keymaps/giatmanos/keymap.c
+++ keyboards/crkbd/keymaps/giatmanos/keymap.c
@@ -35,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_DEL,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    KC_TAB,ALT_T(KC_A),GUI_T(KC_S),SFT_T(KC_D),CTL_T(KC_F),KC_G,                    KC_H,CTL_T(KC_J),SFT_T(KC_K),GUI_T(KC_L),ALT_T(KC_SCLN), KC_QUOT,
+      KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_ESC,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_LALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LBRC,  MO(1),    KC_ENT,     KC_SPC,  MO(2), KC_RBRC
+                                          MO(1),   TD(TD_SFT_CTRL), KC_ENT,     KC_SPC,   TD(TD_CTRL_SFT), MO(2)
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TILD, XXXXXXX, XXXXXXX,  KC_EQL, KC_MINS, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_HOME,  KC_END, KC_PLUS, KC_UNDS, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_PIPE,
+      XXXXXXX, KC_HOME,  KC_END, KC_PLUS, KC_UNDS, XXXXXXX,                      XXXXXXX, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL, KC_LSFT, KC_LALT,   LCA(KC_LEFT), LCA(KC_RIGHT), _______
                                       //`--------------------------'  `--------------------------'
